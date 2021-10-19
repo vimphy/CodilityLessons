41a2d70ac72928505b34f1d4a11a8430a77aec69
Rennan.s.s <vimphy1@gmail.com>
2021-10-19 16:36:17 -0400

    Add files via upload
    Atualizaçáo Frog Jump

--- ColidityVimphy/Frog Jump Simplificada.py
+++ ColidityVimphy/Frog Jump Simplificada.py
@@ -6,13 +6,30 @@ def solution(X, Y, D):
     :param Y: O ponto final minimo
     :param D: Distancia fixa a qual o sapo salta
     """
+    # considerando a distancia total entre X e Y, e dividindo pelo tamanho do pulo
+    # conseguiremos o número de pulos necessários para responder a questão,
+    # sem precisar percorrer a lista várias vezes
+    # Porém se deve ser checado se o valor calculado na formula(v)
+    # multiplcado pelo valor de um único pulo, excede o valor da distancia final Y
+    # Caso seja pode ser que a funçao retorne número de pulos menor que o mínimo
+    # requerido para percorrer a distancia
+    # logo deve ser adicionado +1 ao número de pulos (v) calculados
 
-    # Assuma que X é menor ou igual a Y
-    assert X <= Y
+    # Floor division (//) garante que o calculo dos pulos seja um inteiro não float
+    # (Y - X) referese a distancia entre X e Y sendo X > Y
+    # Dividindo esse valor pela distancia de cada pulo (D) resultará
+    # o numero de pulos necessarios.
+    v = (Y - X) // D
 
-    # Com a utilização da função divmod obtemos o quociente da divisão entre a distancia que precisa ser percorrida
-    # (ou seja, a distancia minima do final menos o ponto inicial)
-    # E o tamnho fixo dos pulos do sapo. Sabem assim quantos pulos devcm ser dados no mínimo (quociente)
-    coef, resto = divmod(Y - X, D)
-    return coef if resto == 0 else coef + 1
+    # é checado se o número de pulos (v) é maior ou igual
+    # a soma da posição inicial do sapo + o número de pulos calculados
+    # multiplicado pela distancia do pulo que foi dada
+    # assim garantindo que se por exemplo x = 1 e Y 6 e a distancia do pulo (D_ seja 4
+    # não haja um valor menor de pulos como resultado
+    if X + v * D >= Y:
+        return v
+    else:
+        return v+1
+pass
 
+print(solution(10, 85, 30))
